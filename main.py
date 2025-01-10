e35d6d28b9072307a2a8ef72a44782e349ae95f4
freeok <51998152+freeok@users.noreply.github.com>
2025-01-11 07:22:27 +0800

    :bug: 修复is_win类型错误

--- main.py
+++ main.py
@@ -3,67 +3,64 @@
 # Author: allenmirac ########
 # Date: 2023-06-18 ##########
 #############################
-import os
-import sys
-import shutil
-from os.path import join, exists
-import urllib.request
-import requests
-import httpx
 import argparse
-import glob
-from bs4 import BeautifulSoup, NavigableString
-from utils import Parser
-import time as tm
+import os
 import re
-from download_img_queue import Download_img_queue
+import shutil
+import time as tm
+from os.path import join, exists
 from queue import Queue
+
+import httpx
+from bs4 import BeautifulSoup, NavigableString
+
+from download_img_queue import Download_img_queue
+from utils import Parser
 
 # 使用argparse库创建了一个命令行参数解析器，并定义了多个命令行参数
 parser = argparse.ArgumentParser('CSDN Blog Exporter: To Markdown or To PDF')
 group = parser.add_mutually_exclusive_group()
 group.add_argument('--category_url', type=str,
-                    help='CSDN Category Url, e.g., https://blog.csdn.net/m0_67623521/category_11879868.html')
+                   help='CSDN Category Url, e.g., https://blog.csdn.net/m0_67623521/category_11879868.html')
 group.add_argument('--article_url', type=str,
-                    help='CSDN Article Url, e.g., https://blog.csdn.net/m0_67623521/article/details/129597894')
+                   help='CSDN Article Url, e.g., https://blog.csdn.net/m0_67623521/article/details/129597894')
 parser.add_argument('--start_page', type=int, default=1,
-                   help='Start Page of CSDN Category')
+                    help='Start Page of CSDN Category')
 parser.add_argument('--page_num', type=int, default=100,
                     help='Page Number of CSDN Category.'
-                    ' If you set this to a large number, all articles in all pages of this category will be downloaded')
+                         ' If you set this to a large number, all articles in all pages of this category will be downloaded')
 parser.add_argument('--markdown_dir', type=str, default='markdown',
-                   help='Markdown Directory')
+                    help='Markdown Directory')
 parser.add_argument('--pdf_dir', type=str, default='pdf',
-                   help='PDF Directory')
+                    help='PDF Directory')
 # parser.add_argument('--figure_dir', type=str, default='figures',
-                   # help='Figures Directory')
+# help='Figures Directory')
 parser.add_argument('--with_title', action='store_true')
 parser.add_argument('--to_pdf', action='store_true')
 parser.add_argument('--rm_cache', action='store_true',
-                   help='remove cached file')
-parser.add_argument('--is_win', 
+                    help='remove cached file')
+parser.add_argument('--is_win',
                     choices=[1, 0], default=0, type=int,
-                   help='platform: windows-1, Linux-0')
+                    help='platform: windows-1, Linux-0')
 parser.add_argument('--combine_together', action='store_true',
-                   help='Combine all markdown file in markdown_dir to a single file.'
-                   ' And if to_pdf, the single file will be converted pdf format')
-args = parser.parse_args() # 保存命令行参数
+                    help='Combine all markdown file in markdown_dir to a single file.'
+                         ' And if to_pdf, the single file will be converted pdf format')
+args = parser.parse_args()  # 保存命令行参数
 
 # 图片下载队列
 download_img_queue = Queue()
-num_workers=5
+num_workers = 5
 img_queue_downloader = Download_img_queue(download_img_queue, True, num_workers)
-
 
 
 def html2md(url, md_file, with_title=False, is_win=True):
     response = httpx.get(url)
     soup = BeautifulSoup(response.content, 'html.parser', from_encoding="utf-8")
     title = soup.find_all('h1', {'class': 'title-article'})[0].string
-    title = '_'.join(title.replace('*', '').strip().split())        # 使用下划线连接单词
+    title = '_'.join(title.replace('*', '').strip().split())  # 使用下划线连接单词
     category = ''
-    if soup.find_all('span', {'class': 'tit'}):                     # 文章归类
-        category = soup.find_all('span', {'class': 'tit'})[0].string    
+    if soup.find_all('span', {'class': 'tit'}):  # 文章归类
+        category = soup.find_all('span', {'class': 'tit'})[0].string
     time = soup.find_all('span', {'class': 'time'})[0].string
     match = re.search(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', time)
     time = match.group()
@@ -87,9 +84,10 @@ def html2md(url, md_file, with_title=False, is_win=True):
         if category != '':
             f.write('tags: ' + category + '\n')
         f.write('---\n')
-        f.write('\n'+ '<meta name="referrer" content="no-referrer" />'+'\n')
+        f.write('\n' + '<meta name="referrer" content="no-referrer" />' + '\n')
     with open(md_file, 'a', encoding="utf-8") as f:
         f.write('{}\n'.format(''.join(parser.outputs)))
+
 
 def generate_pdf(input_md_file, pdf_dir, is_win=True):
     if not exists(pdf_dir):
@@ -100,35 +98,36 @@ def generate_pdf(input_md_file, pdf_dir, is_win=True):
     pdf_file = join(pdf_dir, pdf_name)
     if is_win:
         cmd = ['pandoc',
-            '--toc',
-            '--pdf-engine=xelatex',
-            '-V mainfont="Source Code Pro"',
-            '-V monofont="Source Code Pro"',
-            '-V documentclass="ctexbook"',
-            '-V geometry:"top=2cm, bottom=1cm, left=1.5cm, right=1.5cm"',
-            '-V pagestyle=plain',
-            '-V fontsize=11pt',
-            '-V colorlinks=blue',
-            '-s {}'.format(input_md_file),
-            '-o {}'.format(pdf_file),
-        ]
+               '--toc',
+               '--pdf-engine=xelatex',
+               '-V mainfont="Source Code Pro"',
+               '-V monofont="Source Code Pro"',
+               '-V documentclass="ctexbook"',
+               '-V geometry:"top=2cm, bottom=1cm, left=1.5cm, right=1.5cm"',
+               '-V pagestyle=plain',
+               '-V fontsize=11pt',
+               '-V colorlinks=blue',
+               '-s {}'.format(input_md_file),
+               '-o {}'.format(pdf_file),
+               ]
     else:
         cmd = ["pandoc",
-            "--toc",
-            "--pdf-engine=xelatex",
-            "-V mainfont='Source Code Pro'",
-            "-V monofont='Source Code Pro'",
-            "-V documentclass='ctexart'",
-            "-V geometry:'top=2cm, bottom=1cm, left=1.5cm, right=1.5cm'",
-            "-V pagestyle=plain",
-            "-V fontsize=11pt",
-            "-V colorlinks=blue",
-            "-s {}".format(input_md_file),
-            "-o {}".format(pdf_file),
-        ]
+               "--toc",
+               "--pdf-engine=xelatex",
+               "-V mainfont='Source Code Pro'",
+               "-V monofont='Source Code Pro'",
+               "-V documentclass='ctexart'",
+               "-V geometry:'top=2cm, bottom=1cm, left=1.5cm, right=1.5cm'",
+               "-V pagestyle=plain",
+               "-V fontsize=11pt",
+               "-V colorlinks=blue",
+               "-s {}".format(input_md_file),
+               "-o {}".format(pdf_file),
+               ]
     cmd = ' '.join(cmd)
     print('Generate PDF File: {}'.format(pdf_file))
     os.system(cmd)
+
 
 def get_category_article_info(soup):
     url = soup.find_all('a')[0].attrs['href']
@@ -139,7 +138,9 @@ def get_category_article_info(soup):
             break
     return url, title
 
-def download_csdn_category_url(category_url, md_dir, start_page=1, page_num=100, pdf_dir='pdf', to_pdf=False, is_win=True):
+
+def download_csdn_category_url(category_url, md_dir, start_page=1, page_num=100, pdf_dir='pdf', to_pdf=False,
+                               is_win=True):
     """
     如果想下载某个 category 下的所有页面, 那么 page_num 设置大一些
     """
@@ -184,7 +185,7 @@ def download_csdn_single_page(details_url, md_dir, with_title=True, pdf_dir='pdf
     soup = BeautifulSoup(response.content, 'html.parser', from_encoding="utf-8")
     title = soup.find_all('h1', {'class': 'title-article'})[0].string  ## 使用 html 的 title 作为 md 文件名
     title = '_'.join(title.replace('*', '').strip().split())
-    title = title.replace('/',"或")
+    title = title.replace('/', "或")
     # print(title)
     md_file = join(md_dir, title + '.md')
     print('Export Markdown File To {}'.format(md_file))
@@ -212,8 +213,8 @@ if __name__ == '__main__':
                                    args.markdown_dir,
                                    start_page=args.start_page,
                                    page_num=args.page_num)
-                                #    pdf_dir=args.pdf_dir,
-                                #    to_pdf=args.to_pdf)
+        #    pdf_dir=args.pdf_dir,
+        #    to_pdf=args.to_pdf)
         # download_csdn_category_url(args.category_url,
         #                            args.markdown_dir,
         #                            start_page=args.start_page,
@@ -222,16 +223,16 @@ if __name__ == '__main__':
         #                            to_pdf=args.to_pdf)
     else:
         download_csdn_single_page(args.article_url,
-                                 args.markdown_dir,
-                                 with_title=args.with_title)
-                                #  pdf_dir=args.pdf_dir,
-                                #  to_pdf=args.to_pdf)
+                                  args.markdown_dir,
+                                  with_title=args.with_title)
+        #  pdf_dir=args.pdf_dir,
+        #  to_pdf=args.to_pdf)
         # download_csdn_single_page(args.article_url,
         #                           args.markdown_dir,
         #                           with_title=args.with_title,
         #                           pdf_dir=args.pdf_dir,
         #                           to_pdf=args.to_pdf)
-    is_win = args.is_win == "1"
+    is_win = args.is_win == 1
     if args.combine_together:
         source_files = join(args.markdown_dir, '*.md')
         md_file = 'my_together_all_file.md'
@@ -254,4 +255,4 @@ if __name__ == '__main__':
     img_queue_downloader.stop()
     print("下载文件结束!!!")
     time_end = tm.time()
-    print("Time consume: ", time_end-time_start)
+    print("Time consume: ", time_end - time_start)
